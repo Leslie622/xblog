388c86ee2cb6931d5d043826d88dc43c92759b3a
17671448657 <416317444@qq.com>
2020-10-13 22:37:09 +0800

    关于页起步

--- src/views/about/About.vue
+++ src/views/about/About.vue
@@ -4,7 +4,7 @@
       <div id="container"></div>
       <div class="frame">
         <div class="frame__deco">
-          <!-- <span class="frame__deco-inner">Leslie</span> -->
+          <span class="frame__deco-inner">Leslie</span>
         </div>
       </div>
       <div class="content">
